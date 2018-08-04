a36bd535fa587d75ced4b03dbfeae933aff9d2db
Pekka Enberg <penberg@scylladb.com>
2018-08-04 18:42:06 +0300

    BulkLoader: Don't reuse PreparedStatement instances
    When prepared statements are used with async execution, we must not
    reuse PreparedStatement instances until the async operation is complete.
    Therefore, drop the prepared statement ConcurrentHashMap altogether and
    always build a new PreparedStatement instance. Please note that this
    does *not* mean re-preparing the statement on server side, that's going
    to be reused anyway automatically.

--- src/java/com/scylladb/tools/BulkLoader.java
+++ src/java/com/scylladb/tools/BulkLoader.java
@@ -202,7 +202,7 @@ public class BulkLoader {
         private int bytes;
 
         private final boolean batch;
-        private final Map<String, ListenableFuture<PreparedStatement>> preparedStatements;
+        private final boolean prepare;
         private final Set<String> ignoreColumns;
         private final CodecRegistry codecRegistry = new CodecRegistry();
         private final TypeCodec<ByteBuffer> blob = codecRegistry.codecFor(ByteBuffer.allocate(1));
@@ -276,7 +276,7 @@ public class BulkLoader {
             }
 
             this.batch = options.batch;
-            this.preparedStatements = options.prepare ? new ConcurrentHashMap<>() : null;
+            this.prepare = options.prepare;
             this.ignoreColumns = options.ignoreColumns;
         }
 
@@ -612,7 +612,7 @@ public class BulkLoader {
                 System.out.println();
             }
 
-            if (preparedStatements != null) {
+            if (prepare) {
                 sendPrepared(callback, key, timestamp, what, objects);
             } else {
                 send(callback, key, timestamp, what, objects);
@@ -661,13 +661,9 @@ public class BulkLoader {
 
         private void sendPrepared(final Object callback, final DecoratedKey key, final long timestamp, String what,
                 final List<Object> objects) {
-            ListenableFuture<PreparedStatement> f = preparedStatements.get(what);
-            if (f == null) {
-                if (verbose) {
-                    System.out.println("Preparing: " + what);
-                }
-                f = session.prepareAsync(what);
-                preparedStatements.put(what, f);
+            ListenableFuture<PreparedStatement> f = session.prepareAsync(what);
+            if (verbose) {
+                System.out.println("Preparing: " + what);
             }
 
             try {
